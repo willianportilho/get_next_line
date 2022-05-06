bdf3a10b27002122d4e86b96d7dde276e461636c
wportilh <wportilh@student.42sp.org.br>
2022-05-06 20:17:38 +0200

    first send

---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 20:39:02 by wportilh          #+#    #+#             */
+/*   Updated: 2022/05/06 19:30:32 by wportilh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*cut_act(char *actual);
+char	*line_to_send(char *actual);
+char	*ft_strjoin_alt(char *s1, char *s2);
+int		check_break(char *actual);
+size_t	ft_strlen(char *s);
+
+#endif
